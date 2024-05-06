553339492be45a88b1ebd1309d5c10979eaccf9d
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-05-06 13:24:09 +0200

    add data field to widget tile

--- model/widget_tile.go
+++ model/widget_tile.go
@@ -5,5 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type WidgetTile struct {
 	WidgetID      primitive.ObjectID `json:"-" bson:"widget_id,omitempty"`
 	Widget        Widget             `json:"widget,omitempty" bson:"-"`
+	Data          any                `json:"data,omitempty" bson:"data,omitempty"`
 	OrderPosition int                `json:"order_position,omitempty" bson:"order_position,omitempty"`
 }
