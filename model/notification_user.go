bcf5f506b1794635555bb4539495db28334facce
konrad2002 <konrad@schwimmteamerzgebirge.de>
2024-11-19 19:56:13 +0100

    fix notificaiton user model use omitempty for object ids in database

--- model/notification_user.go
+++ model/notification_user.go
@@ -3,8 +3,8 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type NotificationUser struct {
-	Identifier primitive.ObjectID `json:"_id" bson:"_id"`
-	UserId     primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Identifier primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	UserId     primitive.ObjectID `json:"user_id" bson:"user_id,omitempty"`
 	Token      string             `json:"token" bson:"token"`
 	Settings   Settings           `json:"settings" bson:"settings"`
 	Device     Device             `json:"device" bson:"device"`
