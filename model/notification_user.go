bf6922826056bcb9166127f379fa82c7c534b690
konrad2002 <konrad@schwimmteamerzgebirge.de>
2024-11-26 01:55:01 +0100

    fix notification settings model

--- model/notification_user.go
+++ model/notification_user.go
@@ -9,14 +9,14 @@ type NotificationUser struct {
 	Identifier primitive.ObjectID   `json:"_id" bson:"_id,omitempty"`
 	UserId     primitive.ObjectID   `json:"user_id" bson:"user_id,omitempty"`
 	Token      string               `json:"token" bson:"token"`
-	Settings   NotificationSettings `json:"settings" bson:"settings"`
+	Settings   NotificationSettings `json:"settings,omitempty" bson:"settings"`
 	Device     Device               `json:"device" bson:"device"`
 	AddedAt    time.Time            `json:"added_at,omitempty" bson:"added_at,omitempty"`
 	UpdatedAt  time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
 
 type NotificationSettings struct {
-	Athlete    bool `json:"token" bson:"token"`
+	Athlete    bool `json:"athlete" bson:"athlete"`
 	Favourites bool `json:"favourites" bson:"favourites"`
 	Meeting    bool `json:"meeting" bson:"meeting"`
 	Schedule   bool `json:"schedule" bson:"schedule"`
