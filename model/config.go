0e00c4fd942b380ade2d4ef5b2a695fb6f9e0034
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-12-07 08:29:05 +0100

    always show enabled

--- model/config.go
+++ model/config.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Config struct {
 	Identifier primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Meeting    string             `json:"meeting,omitempty" bson:"meeting,omitempty"`
-	Enabled    bool               `json:"enabled,omitempty" bson:"enabled,omitempty"`
+	Enabled    bool               `json:"enabled" bson:"enabled"`
 }
